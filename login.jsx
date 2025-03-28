92223ba44e0b222a78ad7d6f77bb3c648b5b278e
HJ429 <pl980918@naver.com>
2025-03-28 17:26:52 +0900

    Update login.jsx

--- login.jsx
+++ login.jsx
@@ -29,28 +29,14 @@ const LoginPage = () => {
     return (
         <div className="login-total-box">
             <div className="login-container">
-            <h2>카페연구실 로그인</h2>
+            <h2 className="login-h2">카페연구실 로그인</h2>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="아이디"
-                    required
-                />
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="비밀번호"
-                    required
-                />
-                <input type="submit" value="로그인" />
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="아이디" required/>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" required/>
+                <input type="submit" id="submit" value="로그인" />
                 {errorMessage && <div className="error">{errorMessage}</div>}
             </form>
-                <div className="footer">
+                <div className="login-pont">
                     <a href="cafemain.jsx" id="main-link">
                         메인으로
                     </a>
