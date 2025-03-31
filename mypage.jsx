6fd30547a71020fcd67b3c98b6fe77f08d242d80
HJ429 <pl980918@naver.com>
2025-03-31 17:29:04 +0900

    Add files via upload

---
+++ mypage.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./MyPage.css"; // 별도의 CSS 파일로 스타일링을 분리합니다.
+
+const MyPage = () => {
+  const showChangePassword = () => {
+    // 비밀번호 변경 로직
+    console.log("비밀번호 변경");
+  };
+
+  const showChangeDob = () => {
+    // 생년월일 수정 로직
+    console.log("생년월일 수정");
+  };
+
+  const showChangeAddress = () => {
+    // 주소 변경 로직
+    console.log("주소 변경");
+  };
+
+  const deleteAccount = () => {
+    // 회원 탈퇴 로직
+    console.log("회원 탈퇴");
+  };
+
+  return (
+    <div className="mypage-total-box">
+      <div className="mypage-box-h1">
+        <h1 className="mypage-h1">My Page</h1>
+      </div>
+      <section className="mypage-box-section">
+        <div className="mypage-box">
+          <div className="mypage-img-box-box">
+            <div className="mypage-img-box">
+              <img className="mypage-img" src="./pit/" alt="사진" />
+            </div>
+          </div>
+          <hr className="mypage-hr-1" />
+          <div className="mypage-p-box">
+            <p className="mypage-p">아이디 : hotdog</p>
+            <span className="mypage-username">사용자이름 : 비비빅</span>
+          </div>
+          <hr className="mypage-hr" />
+          <div className="mypage-pwd-box">
+            <p className="mypage-p-pwd">
+              비밀번호 : 12344
+              <button className="mypage-btn-pwd" onClick={showChangePassword}>
+                비밀번호 변경
+              </button>
+            </p>
+          </div>
+          <hr className="mypage-hr" />
+          <p className="mypage-p-bri">
+            생년월일 : 2025-03-28
+            <button className="mypage-btn-bri" onClick={showChangeDob}>
+              생년월일 수정
+            </button>
+          </p>
+          <hr className="mypage-hr" />
+          <p className="mypage-p-adr">
+            주소 : 부산 부산진구 양정동 쩐다빌라 204호
+            <button className="mypage-btn-adr" onClick={showChangeAddress}>
+              주소 변경
+            </button>
+          </p>
+          <div className="mypage-bye-box">
+            <button className="mypage-btn-bye" onClick={deleteAccount}>
+              회원 탈퇴
+            </button>
+          </div>
+        </div>
+
+        <hr className="left-hr" />
+        <div className="mypage-posts-box">
+          <div className="empty-box"></div>
+          <div className="mypage-posts">
+            <h2 className="mypage-h2">내가 쓴 글</h2>
+            <div className="mypage-posts-list">
+              ...
+              {/* 내가 쓴 글 리스트가 동적으로 여기에 표시됨 */}
+            </div>
+          </div>
+          <hr className="mypage-hr2" />
+          <div className="mypage-comments">
+            <h2 className="mypage-h2">내가 쓴 댓글</h2>
+            <div className="mypage-comments-list">
+              ...
+              {/* 내가 쓴 댓글 리스트가 동적으로 여기에 표시됨 */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MyPage;
