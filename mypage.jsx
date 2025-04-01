d287733b39b8c52cb2f3c14e2471a31401ece927
HJ429 <pl980918@naver.com>
2025-04-01 17:30:31 +0900

    Update mypage.jsx

--- mypage.jsx
+++ mypage.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./MyPage.css"; // 별도의 CSS 파일로 스타일링을 분리합니다.
 import { useNavigate } from "react-router-dom";
 
+
 const MyPage = () => {
+
+  const [imageSrc, setImageSrc] = useState("src/pit/comm.jpg"); // 기본 이미지 경로
+
   const showChangePassword = () => {
     // 비밀번호 변경 로직
     console.log("비밀번호 변경");
@@ -18,7 +22,23 @@ const MyPage = () => {
     console.log("주소 변경");
   };
 
-  const navigate = useNavigate();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result); // 선택한 이미지를 미리보기로 표시
+      };
+      reader.readAsDataURL(file); // 파일 읽기
+    }
+  };
+
+  // 아이콘 클릭 시 파일 선택 다이얼로그 열기
+  const handleIconClick = () => {
+    document.getElementById("file-input").click();
+  };
+
+  // const navigate = useNavigate();
   const deleteAccount = () => {
     // 회원 탈퇴 로직
     const confirmDelete = window.confirm("정말 탈퇴하시겠습니까?");
@@ -30,7 +50,7 @@ const MyPage = () => {
       alert("탈퇴되셨습니다.");
       
       // 메인 화면으로 이동 (여기서는 '/'가 메인 페이지로 가정)
-      navigate('/');
+      // navigate('/');
     } else {
       console.log("회원 탈퇴 취소");
     }
@@ -44,14 +64,16 @@ const MyPage = () => {
       <section className="mypage-box-section">
         <div className="mypage-box">
           <div className="mypage-img-box-box">
+          <img className="mypage-img-icon" src="src/pit/img icon2.png" alt="아이콘" onClick={handleIconClick} />
             <div className="mypage-img-box">
               <img className="mypage-img" src="src/pit/comm.jpg" alt="사진" />
+
             </div>
+            <p className="mypage-img-box-p">비비빅 님! 환영합니다!</p>
           </div>
           <hr className="mypage-hr-1" />
           <div className="mypage-p-box">
             <p className="mypage-p">아이디 : hotdog</p>
-            <span className="mypage-username">사용자이름 : 비비빅</span>
           </div>
           <hr className="mypage-hr" />
           <div className="mypage-pwd-box">
@@ -66,7 +88,7 @@ const MyPage = () => {
           <p className="mypage-p-bri">
             생년월일 : 2025-03-28
             <button className="mypage-btn-bri" onClick={showChangeDob}>
-              생년월일 수정
+              생년월일 변경
             </button>
           </p>
           <hr className="mypage-hr" />
@@ -83,7 +105,7 @@ const MyPage = () => {
           </div>
         </div>
         <div className="mypage-posts-box">
-          <div className="empty-box"></div>
+          {/* <div className="empty-box"></div> */}
           <div className="mypage-posts">
             <h2 className="mypage-h2">내가 쓴 글</h2>
             <div className="mypage-posts-list">
